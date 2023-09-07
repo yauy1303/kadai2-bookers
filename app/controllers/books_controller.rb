a7b45d0bc0f17d2c115387967148393719582ebb
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-09-07 12:05:09 +0000

    bookのcreate機能の作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def index
+    @book = Book.new
+    @book = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
